70bb0dcd254460e863f707b724c70ec9d81efb18
VadimSK <vadim.kuntzevich@gmail.com>
2021-07-04 15:34:09 +0800

    Rebuild tutor project with Maven

---
+++ target/gwt/devmode/work/com.google.gwt.sample.stockwatcher.StockWatcher/compile-2/gen/com/google/gwt/lang/com_00046google_00046gwt_00046sample_00046stockwatcher_00046StockWatcher__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046google_00046gwt_00046sample_00046stockwatcher_00046StockWatcher__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
